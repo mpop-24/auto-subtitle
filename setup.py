1d9c522f04608006c4be42de028b84b23177cbb6
mpop-24 <137960555+mpop-24@users.noreply.github.com>
2025-02-15 15:22:02 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     version="1.0",
-    name="subtitle",
+    name="generate_subtitle",
     packages=find_packages(),
-    py_modules=["subtitle"],
+    py_modules=["generate_subtitle"],
     author="Markell Popov",
     install_requires=[
         'openai-whisper',
     ],
     description="Automatically generate and embed subtitles into your videos",
     entry_points={
-        'console_scripts': ['subtitle=subtitle.cli:main'],
+        'console_scripts': ['gen_subtitle=subtitle.cli:main'],
     },
     include_package_data=True,
 )
